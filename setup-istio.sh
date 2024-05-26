6fd0447d25497e471d4ecf7c80e03d5f7c01eec9
Tan Luu <luuhoangtanbis@gmail.com>
2024-05-27 01:02:35 +0700

    Initial

---
+++ setup-istio.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+CLUSTER_NETWORK=""
+CLUSTER="Kubernetes" #This is a fixed value
+
+istioctl operator init
+
+cat <<EOF > vm-cluster.yaml
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+metadata:
+  name: istio
+spec:
+  values:
+    global:
+      meshID: mesh1
+      multiCluster:
+        clusterName: "${CLUSTER}"
+      network: "${CLUSTER_NETWORK}"
+  meshConfig:
+    accessLogFile: /dev/stdout
+EOF
+
+istioctl install -f vm-cluster.yaml -y
